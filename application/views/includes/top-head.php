9413fea54d67575ed26e7bc367e2eb7b10c25dea
softisoDev <ismayil.developer@gmail.com>
2020-02-22 18:05:45 +0400

    fix header image issue

--- application/views/includes/top-head.php
+++ application/views/includes/top-head.php
@@ -203,7 +203,7 @@
                     <li class="dropdown dropdown-user nav-item">
                         <a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
                 <span class="avatar avatar-online">
-                  <img src="app-assets/images/portrait/small/avatar-s-1.png" alt="avatar"><i></i>
+                  <img src="<?php echo base_url("app-assets/images/portrait/small/avatar-s-1.png"); ?>" alt="avatar"><i></i>
                 </span>
                             <span class="mr-1">Hello,
                   <span class="user-name text-bold-700"><?php echo $_SESSION['user']->name; ?></span>
